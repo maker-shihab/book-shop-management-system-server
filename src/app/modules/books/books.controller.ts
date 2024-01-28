b9bf84acae7f8e0df7340b14037193022dce4295
maker-shihab <frontendmaker99@gmail.com>
2024-01-29 01:54:09 +0600

    Inprove CatchAsync

--- src/app/modules/books/books.controller.ts
+++ src/app/modules/books/books.controller.ts
@@ -1,1 +1,4 @@
-// const createBook: RequestHandler = catchAsync() => {}
+import { RequestHandler } from "express";
+import catchAsync from "../../../shared/catchAsync";
+
+const createBook: RequestHandler = catchAsync();
