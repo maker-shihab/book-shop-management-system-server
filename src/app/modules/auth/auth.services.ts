c65230869736e558a389d27c52ecac4837d2b716
maker-shihab <frontendmaker99@gmail.com>
2024-02-05 23:57:27 +0600

    add auth service with update functions

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -5,7 +5,7 @@ const login = async (identifyer: string) => {
   if (!identifyer) {
     throw new ApiError(httpStatus.NOT_FOUND, "Not found login information");
   }
-  // const { password, userName, email } = identifyer;
+  const { password, userName, email } = identifyer;
 };
 
 export const authServices = {
