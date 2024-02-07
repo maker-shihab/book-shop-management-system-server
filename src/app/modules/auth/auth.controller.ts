6ecc59ebf09ccaa04ad91fa82073c79046c41ebe
maker-shihab <frontendmaker99@gmail.com>
2024-02-07 23:54:04 +0600

    add auth service with update functions

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,9 +1,18 @@
+import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
 import { authServices } from "./auth.services";
 
 const login = catchAsync(async (req, res) => {
   const { data } = req.body;
   const result = authServices.login(data);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Login successfull",
+    data: result,
+  });
   return result;
 });
 
