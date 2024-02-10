32943d02ee879076f9a42fda5a5b2673309b1822
maker-shihab <frontendmaker99@gmail.com>
2024-02-10 23:48:37 +0600

    add auth with update functions

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import catchAsync from "../../../shared/catchAsync";
 
 const login = catchAsync(async (req, res) => {
   const { data } = req.body;
+  const result = authServices.login(data);
   return data;
 });
 
